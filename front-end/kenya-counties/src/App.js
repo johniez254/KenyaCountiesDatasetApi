b29b18cb2344a92ff306ad64564970977c5789e9
johniez254 <johnsonmatoke@gmail.com>
2022-12-23 16:51:12 +0300

    modified UI

--- front-end/kenya-counties/src/App.js
+++ front-end/kenya-counties/src/App.js
@@ -1,15 +1,29 @@
+import React from 'react'
 import logo from './logo.svg'
-import './App.css'
-import counties from './data'
+import data from './data'
 import List from './List'
+import { useState } from 'react'
 
 function App() {
-  return (
-    <main>
-      <section className='container'>
-        <List countiesData={counties} />
+  const [loading, setLoading] = useState(false)
+  const [counties, setCounties] = useState(data)
+
+  if (loading) {
+    return (
+      <section className='section loading'>
+        <h1>loading...</h1>
       </section>
-    </main>
+    )
+  }
+
+  return (
+    <section className='section'>
+      <div className='title'>
+        <h2>counties {counties.length}</h2>
+        <div className='underline'></div>
+      </div>
+      <List countiesData={counties} />
+    </section>
   )
 }
 
--- front-end/kenya-counties/src/List.js
+++ front-end/kenya-counties/src/List.js
@@ -1,31 +1,81 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaAngleDoubleRight, FaHotel } from 'react-icons/fa'
 
 const List = ({ countiesData }) => {
-  const [counties, setCounties] = React.useState(countiesData)
+  const [defaultCounty, setDefaultCounty] = useState(countiesData)
+  const [value, setValue] = useState(0)
+  const [isToggled, setIsToggled] = useState(false)
+  const [id, setId] = useState(null)
+
+  const getCounty = (id) => {
+    setValue(id - 1)
+  }
+
+  const displayWard = (id) => {
+    setIsToggled(!isToggled)
+    setId(id)
+  }
+
+  const {
+    countyName,
+    countyCode,
+    totalConstituencies,
+    totalWards,
+    constituencies,
+  } = defaultCounty[value]
   return (
     <>
-      <h3>{counties.length} total counties</h3>
-      {counties.map((county) => {
-        const {
-          county_id,
-          countyName,
-          countyCode,
-          totalConstituencies,
-          totalWards,
-        } = county
-        return (
-          <article className='person' key={county_id}>
-            <div>
-              <h4>
-                {county_id}) {countyName}
-              </h4>
-              <h5>Code: {countyCode}</h5>
-              <p>Constituencies: {totalConstituencies}</p>
-              <p>Wards: {totalWards}</p>
-            </div>
-          </article>
-        )
-      })}
+      <div className='jobs-center'>
+        <div className='btn-container'>
+          {countiesData.map((county, index) => {
+            return (
+              <button
+                key={county.county_id}
+                className={`job-btn ${index === value && 'active-btn'}`}
+                onClick={() => getCounty(county.county_id)}
+              >
+                {county.countyName}
+              </button>
+            )
+          })}
+        </div>
+        <article className='job-info'>
+          <h3>{countyName}</h3>
+          <h4>{countyCode}</h4>
+
+          <h5>Constituencies {constituencies.length}</h5>
+          {constituencies.map((constituency, index) => {
+            const {
+              constituency_id,
+              constituencyName,
+              constituencyCode,
+              wards,
+            } = constituency
+            return (
+              <div
+                className='job-desc'
+                key={constituency_id}
+                onClick={() => displayWard(constituency_id)}
+              >
+                <FaAngleDoubleRight className='job-icon' />
+                <p>
+                  <span>({constituencyCode})</span> {constituencyName}
+                </p>
+                {wards.map((ward) => {
+                  const { ward_id, wardName, wardCode, wardConstituencyId } =
+                    ward
+
+                  return (
+                    <p className='wards' key={ward_id}>
+                      {isToggled && wardConstituencyId === id ? wardName : ''}
+                    </p>
+                  )
+                })}
+              </div>
+            )
+          })}
+        </article>
+      </div>
     </>
   )
 }
